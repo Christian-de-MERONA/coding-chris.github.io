d8cf4a80f932cb2d2870a21c9129e833b38ba980
Christian-de-MERONA <christian.de.merona@gmail.com>
2021-02-09 09:26:13 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to my coding portfolio</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, corporis. Repudiandae sunt saepe soluta tempore esse? Deserunt quae accusantium consectetur, totam placeat quia libero exercitationem? Dolore perspiciatis culpa veritatis ex similique incidunt laudantium repellendus molestias vitae accusantium nostrum, praesentium sed suscipit, modi aspernatur voluptas consequuntur. Inventore architecto nobis nemo accusantium!</p>
+</body>
+</html>
