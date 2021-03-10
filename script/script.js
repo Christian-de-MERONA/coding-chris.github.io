f85ac9fcc36870c29bd313b7441fca0a49e8b013
Christian-de-MERONA <christian.de.merona@gmail.com>
2021-03-10 14:33:14 +0000

    sections added to DOM with script + animation on list and title hover

--- script/script.js
+++ script/script.js
@@ -1,30 +1,77 @@
 let app = {
     init: function(){
-        let sectionList = document.querySelectorAll('.section');
-
-        for (section of sectionList){
-            section.addEventListener("mouseover", app.showList);
-            section.addEventListener("mouseleave", app.hideList);
-        }
-
-        console.log(sectionInfo);
-
+        app.addSectionsToDOM();
 
     },
     showList:function(evt){
         const mouseoverElement = evt.target;
-        if(mouseoverElement.classList.contains("section")){
-            const listElement = mouseoverElement.querySelector(".section__list");
-            listElement.classList.remove("list-hidden");
-        }
+        mouseoverElement.closest(".section").classList.remove("list-hidden");
     },
     hideList:function(evt){
         const mouseoverElement = evt.target;
-        if(mouseoverElement.classList.contains("section")){
-            const listElement = mouseoverElement.querySelector(".section__list");
-            listElement.classList.add("list-hidden");
+        mouseoverElement.closest(".section").classList.add("list-hidden");
+
+    },
+    addSectionsToDOM:function(){
+        // get sections div
+        const sectionsBlock = document.querySelector("#sections");
+
+        // for each section info
+        for (section of sectionsInfo){
+            // get section template
+            let sectionTemplate = document.querySelector("#section-template");
+            
+            // clone template
+            let newSection = sectionTemplate.content.cloneNode(true);
+
+            // get title element of section template
+            let sectionTitle = newSection.querySelector(".section__title");
+
+            // get list element of section template
+            let sectionList = newSection.querySelector(".section__list");
+            sectionTitle.textContent = section.title;
+
+            //  for each element in listElements
+            for (listItem of section.listElements){
+                // create li element
+                let sectionListItem = document.createElement("li");
+
+                // add text to li element
+                sectionListItem.innerText = listItem;
+
+                // add li element to section list element
+                sectionList.appendChild(sectionListItem);
+            }
+
+            // create last list element
+            let sectionListItem = document.createElement("li");
+
+            //  add text to last list element
+            sectionListItem.innerText = "...";
+
+            // add li element to section list element
+            sectionList.appendChild(sectionListItem);
+
+            // add new section to sections element
+            sectionsBlock.appendChild(newSection);
+        }
+
+        // call listener function to add listeners to all sections
+        app.addAllListeners();
+
+
+    },
+    addAllListeners:function(){
+        // get all sections 
+        let sectionList = document.querySelectorAll('.section');
+
+        // add listeners to each section
+        for (section of sectionList){
+            section.addEventListener("mouseover", app.showList);
+            section.addEventListener("mouseleave", app.hideList);
         }
     }
+
     
 };
 
--- script/sections.js
+++ script/sections.js
@@ -1,18 +1,66 @@
-let sectionInfo = {
-    1:{
-        "HTML":[
+let sectionsInfo = [
+    {
+        title:"HTML",
+        listElements:[
             "Semantics",
             "File links",
             "Use of 3rd party elements like fonts"
         ]
     },
-    2:{
-        "CSS":[
+    {
+        title:"CSS",
+        listElements:[
             "Responsiveness",
             "Aiming classes",
             "Styling",
             "Positioning"
         ]
     },
-}
+    {
+        title:"Git & Github",
+        listElements:[
+            "Creating a repository",
+            "Cloning an existing repository",
+            "Commiting changes",
+            "Navigating across commits",
+            "Creating, deleting and merging branches"
+        ]
+    },
+    {
+        title:"WireFrames",
+        listElements:[
+            "Prototyping websites"
+        ]
+    },
+    {
+        title:"BootStrap",
+        listElements:[
+            "Quick setup of site with basic stylings"
+        ]
+    },
+    {
+        title:"PHP",
+        listElements:[
+            "Loops",
+            "Functions",
+            "Dynamically building pages",
+            "Classes",
+            "Objects",
+            "MVC",
+            "Handling GET and POST requests with Altrorouter",
+            "Following Psr4 standards"
+        ]
+    },
+    {
+        title:"SQL",
+        listElements:[
+            "Setting up a database with adminer",
+            "Requesting data",
+            "Updating and adding to tables",
+            "Filtering user inputs to sql injections"
+        ]
+    },
+ 
+ 
+]
 
